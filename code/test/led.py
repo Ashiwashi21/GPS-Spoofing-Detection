33dd97d060345652c45a44fe4ce19f285b66e246
Ashirvad Manickandan <ashiwashi21@gmail.com>
2025-10-18 04:09:37 +0100

    new csv files + changed code from data collection to sensor testing and collected raw data

--- code/test/led.py
+++ code/test/led.py
@@ -6,12 +6,12 @@ led = LED(17)
 
 print("Starting LED test...")
 
-for i in range(10):
+for i in range(20):
     led.on()
     print(f"Cycle {i+1}: LED ON")
-    time.sleep(0.5)
+    time.sleep(0.3)
     led.off()
     print(f"Cycle {i+1}: LED OFF")
-    time.sleep(0.5)
+    time.sleep(0.3)
 
 print("LED test complete.")
